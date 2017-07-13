043753762e0db107dcd4a87210f01379d2d267e9
Guillermo Arellano <guillermo.arellanojr@gmail.com>
2017-07-13 15:06:50 -0500

    Created new Contacts-List module, implemented app-routing, created a Page Not Found route component.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
+
+import { ContactsListComponent } from './contacts-list/contacts-list.component';
+import { PageNotFoundComponent } from './page-not-found.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ContactsListComponent,
+    PageNotFoundComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
