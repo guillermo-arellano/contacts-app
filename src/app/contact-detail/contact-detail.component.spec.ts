3407e05bf46c516a5856e7fa852a2135d5718ccb
Guillermo Arellano <guillermo.arellanojr@gmail.com>
2017-07-25 12:50:08 -0500

    Baselined 5 of the main components/services so that the Jasmine unit tests would work on unit creation. Updated zone.js to address Jasmine unit testing bug.

--- src/app/contact-detail/contact-detail.component.spec.ts
+++ src/app/contact-detail/contact-detail.component.spec.ts
@@ -1,16 +1,31 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { HttpModule } from '@angular/http';
+import { RouterTestingModule } from '@angular/router/testing';
+import { Observable } from 'rxjs/Observable';
+import 'rxjs/add/operator/do';
 
+import { ContactsService } from '../contacts.service';
 import { ContactDetailComponent } from './contact-detail.component';
 
 describe('ContactDetailComponent', () => {
   let component: ContactDetailComponent;
   let fixture: ComponentFixture<ContactDetailComponent>;
+  let service: ContactsService,
+      mockHttp;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ ContactDetailComponent ]
+      declarations: [ ContactDetailComponent ],
+      imports: [
+        RouterTestingModule,
+        HttpModule
+      ],
+      providers: [
+        ContactsService
+      ],
     })
     .compileComponents();
+
   }));
 
   beforeEach(() => {
